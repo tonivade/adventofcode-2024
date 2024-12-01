2af9d5d4dc9020d869872e417aa504096ae2c896
Antonio Muñoz <antoniogmc@gmail.com>
2024-12-01 08:57:15 +0100

    day1 part1

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -4,7 +4,14 @@ import scala.io.Source
 
 // https://adventofcode.com/2024/day/1
 object Day1:
-  def part1(input: String): Int = ???
+  def part1(input: String): Int = 
+    val (leftResult, rightResult) = input.linesIterator.map(_.split("   ")).foldLeft((List.empty[Int], List.empty[Int])) {
+      case ((leftList, rightList), Array(left, right)) => (left.toInt :: leftList, right.toInt :: rightList)
+    }
+    (leftResult.sorted zip rightResult.sorted).map {
+      case (left, right) => Math.abs(left - right)
+    }.sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day1Suite.scala
+++ src/test/scala/Day1Suite.scala
@@ -4,10 +4,15 @@ import Day1._
 
 class Day1Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """3   4
+                |4   3
+                |2   5
+                |1   3
+                |3   9
+                |3   3""".stripMargin
 
   test("Day1 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 11)
   }
 
   test("Day1 part2") {

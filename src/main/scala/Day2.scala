1f3c8e4fcb5d298cce4ac4fd61162387b18c94e5
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-02 10:06:11 +0100

    day2 part1

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -4,7 +4,20 @@ import scala.io.Source
 
 // https://adventofcode.com/2024/day/2
 object Day2:
-  def part1(input: String): Int = ???
+
+  def parse(input: String): Iterator[List[Int]] =
+    input.linesIterator.map(_.split(" ")).map {
+      line => line.map(_.toInt).sliding(2).map {
+        case Array(a, b) => a - b
+      }.toList
+    }
+
+  def part1(input: String): Int = 
+    parse(input)
+      .filterNot(report => report.exists(Math.abs(_) > 3))
+      .filter(report => report.forall(_ > 0) || report.forall(_ < 0))
+      .size
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day2Suite.scala
+++ src/test/scala/Day2Suite.scala
@@ -4,10 +4,15 @@ import Day2._
 
 class Day2Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """7 6 4 2 1
+                |1 2 7 8 9
+                |9 7 6 2 1
+                |1 3 2 4 5
+                |8 6 4 4 1
+                |1 3 6 7 9""".stripMargin
 
   test("Day2 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 2)
   }
 
   test("Day2 part2") {

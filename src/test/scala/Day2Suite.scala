dd09d4da01a2ececb4e50c61a3f8f830a9a3cb20
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-02 09:26:31 +0100

    day2 scafolding

---
+++ src/test/scala/Day2Suite.scala
@@ -0,0 +1,16 @@
+package day2
+
+import Day2._
+
+class Day2Suite extends munit.FunSuite:
+
+  val input = """""".stripMargin
+
+  test("Day2 part1") {
+    assertEquals(part1(input), 1)
+  }
+
+  test("Day2 part2") {
+    assertEquals(part2(input), 1)
+  }
+

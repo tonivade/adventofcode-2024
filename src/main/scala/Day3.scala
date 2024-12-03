50c72a518ddd60367d9f9fde30120570c2661733
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-03 09:25:25 +0100

    day3 part2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -7,10 +7,19 @@ object Day3:
   def part1(input: String): Int = 
     val regex = """mul\((\d+),(\d+)\)""".r
     regex.findAllMatchIn(input).map:
-      mul => mul.group(1).toInt * mul.group(2).toInt
+      m => m.group(1).toInt * m.group(2).toInt
     .sum
 
-  def part2(input: String): Int = ???
+  def part2(input: String): Int = 
+    val regex = """do(?:n't)?\(\)|mul\((\d+),(\d+)\)""".r
+    regex.findAllMatchIn(input).foldLeft((true, 0)):
+      (state, m) =>
+        m.group(0) match
+          case "do()" => (true, state._2)
+          case "don't()" => (false, state._2)
+          case _ if state._1 => (state._1, state._2 + (m.group(1).toInt * m.group(2).toInt))
+          case _ => state
+    ._2
 
 @main def main: Unit =
   val input = Source.fromFile("input/day3.txt").getLines().mkString("\n")
--- src/test/scala/Day3Suite.scala
+++ src/test/scala/Day3Suite.scala
@@ -4,13 +4,14 @@ import Day3._
 
 class Day3Suite extends munit.FunSuite:
 
-  val input = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""".stripMargin
+  val input1 = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+  val input2 = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
 
   test("Day3 part1") {
-    assertEquals(part1(input), 161)
+    assertEquals(part1(input1), 161)
   }
 
   test("Day3 part2") {
-    assertEquals(part2(input), 1)
+    assertEquals(part2(input2), 48)
   }
 

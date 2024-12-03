ecf9218097cdaff61b841fdba0923a410ab4271c
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-03 09:04:10 +0100

    day3 part1

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -4,7 +4,12 @@ import scala.io.Source
 
 // https://adventofcode.com/2024/day/3
 object Day3:
-  def part1(input: String): Int = ???
+  def part1(input: String): Int = 
+    val regex = """mul\((\d+),(\d+)\)""".r
+    regex.findAllMatchIn(input).map:
+      mul => mul.group(1).toInt * mul.group(2).toInt
+    .sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day3Suite.scala
+++ src/test/scala/Day3Suite.scala
@@ -4,10 +4,10 @@ import Day3._
 
 class Day3Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""".stripMargin
 
   test("Day3 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 161)
   }
 
   test("Day3 part2") {

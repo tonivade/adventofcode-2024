ee01cdeb88fb223f0f1d72dfd20ebf7ac6779ceb
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-04 08:46:51 +0100

    day4 scafolding

---
+++ src/main/scala/Day4.scala
@@ -0,0 +1,14 @@
+package day4
+
+import scala.io.Source
+
+// https://adventofcode.com/2024/day/4
+object Day4:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day4.txt").getLines().mkString("\n")
+  println(Day4.part1(input))
+  println(Day4.part2(input))
+
---
+++ src/test/scala/Day4Suite.scala
@@ -0,0 +1,16 @@
+package day4
+
+import Day4._
+
+class Day4Suite extends munit.FunSuite:
+
+  val input = """""".stripMargin
+
+  test("Day4 part1") {
+    assertEquals(part1(input), 1)
+  }
+
+  test("Day4 part2") {
+    assertEquals(part2(input), 1)
+  }
+

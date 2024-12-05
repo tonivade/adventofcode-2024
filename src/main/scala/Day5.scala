5b19f1b866ec1752404ca1ff3282b7e6b21b599d
Antonio Muñoz <antoniogmc@gmail.com>
2024-12-05 11:43:17 +0100

    day5

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -4,8 +4,42 @@ import scala.io.Source
 
 // https://adventofcode.com/2024/day/5
 object Day5:
-  def part1(input: String): Int = ???
-  def part2(input: String): Int = ???
+
+  case class Rule(a: Int, b: Int):
+    def check(update: List[Int]): Boolean = 
+      if (update.contains(a) && update.contains(b))
+        update.indexOf(a) < update.indexOf(b)
+      else
+        true
+
+    def eq(x: Int, y: Int): Boolean = a == x && b == y
+
+  def parseRules(input: String): List[Rule] =
+    input.linesIterator.map(_.split("\\|")).map:
+      case Array(a, b) => Rule(a.toInt, b.toInt)
+    .toList
+
+  def parseUpdates(input: String): List[List[Int]] =
+    input.linesIterator.map(_.split(",").map(_.toInt).toList).toList
+
+  def parse(input: String): (List[Rule], List[List[Int]]) =
+    input.split("\n\n") match
+      case Array(part1, part2) => (parseRules(part1), parseUpdates(part2))
+
+  def part1(input: String): Int =
+    val (rules, updates) = parse(input)
+
+    updates.filter(update => rules.forall(_.check(update)))
+      .map(update => update(update.size / 2))
+      .sum
+
+  def part2(input: String): Int = 
+    val (rules, updates) = parse(input)
+    
+    updates.filterNot(update => rules.forall(_.check(update)))
+      .map(_.sortWith((a, b) => rules.exists(_.eq(a, b))))
+      .map(update => update(update.size / 2))
+      .sum
 
 @main def main: Unit =
   val input = Source.fromFile("input/day5.txt").getLines().mkString("\n")
--- src/test/scala/Day5Suite.scala
+++ src/test/scala/Day5Suite.scala
@@ -4,13 +4,61 @@ import Day5._
 
 class Day5Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """47|53
+                |97|13
+                |97|61
+                |97|47
+                |75|29
+                |61|13
+                |75|53
+                |29|13
+                |97|29
+                |53|29
+                |61|53
+                |97|53
+                |61|29
+                |47|13
+                |75|47
+                |97|75
+                |47|61
+                |75|61
+                |47|29
+                |75|13
+                |53|13
+                |
+                |75,47,61,53,29
+                |97,61,53,29,13
+                |75,29,13
+                |75,97,47,61,53
+                |61,13,29
+                |97,13,75,29,47""".stripMargin
+
+  test("check rule") {
+    val update = List(75, 47, 61, 53, 29)
+    val rule = Rule(97, 28)
+
+    assert(rule.check(update))
+  }
+
+  test("check rule not contain") {
+    val update = List(75, 47, 61, 53, 29)
+    val rule = Rule(47, 53)
+
+    assert(rule.check(update))
+  }
+
+  test("not check rule not contain") {
+    val update = List(75, 47, 61, 53, 29)
+    val rule = Rule(53, 47)
+
+    assert(!rule.check(update))
+  }
 
   test("Day5 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 143)
   }
 
   test("Day5 part2") {
-    assertEquals(part2(input), 1)
+    assertEquals(part2(input), 123)
   }
 

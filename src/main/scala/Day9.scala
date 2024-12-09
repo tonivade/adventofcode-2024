0a6429d53cb7b38cbd8cbb6510e874e827edaabc
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-09 11:52:41 +0100

    day9 par1

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -1,10 +1,57 @@
 package day9
 
 import scala.io.Source
+import scala.util.chaining._
+import scala.annotation.tailrec
 
 // https://adventofcode.com/2024/day/9
 object Day9:
-  def part1(input: String): Int = ???
+
+  enum Sector:
+    case File(id: Long)
+    case Free
+    def isFree: Boolean = this match
+      case Free => true
+      case _ => false
+    def repeat(times: Int): List[Sector] = 
+      if (times == 0)
+        Nil
+      else
+        (1 to times).map(_ => this).toList
+
+  import Sector._
+
+  def expand(input: String): List[Sector] = 
+    input.zipWithIndex.foldLeft(0L, List.empty[Sector]):
+      case ((fileId, result), (ch, i)) =>
+        if (i % 2 == 0)
+          (fileId + 1, result ++ File(fileId).repeat(ch.toInt - 48))
+        else
+          (fileId, result ++ Free.repeat(ch.toInt - 48))
+    ._2
+
+  @tailrec
+  def compact(input: List[Sector]): List[Sector] = 
+    val freePosition = input.zipWithIndex.find(_._1.isFree).map(_._2).get
+    val lastSector = input.zipWithIndex.findLast(!_._1.isFree).map(_._2).get
+
+    if (freePosition > lastSector)
+      input
+    else
+      val buffer = input.toBuffer
+      buffer(freePosition) = buffer(lastSector)
+      buffer(lastSector) = Free
+      compact(buffer.toList)
+
+  def checksum(input: List[Sector]): Long = 
+    input.zipWithIndex.map:
+      case (File(id), i) => id * i
+      case _ => 0
+    .sum
+
+  def part1(input: String): Long = 
+    input pipe expand pipe compact pipe checksum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day9Suite.scala
+++ src/test/scala/Day9Suite.scala
@@ -4,10 +4,32 @@ import Day9._
 
 class Day9Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = "2333133121414131402"
+
+  def parse(input: String): List[Sector] =
+    input.map:
+      case ch if ch.isDigit => Sector.File(ch.toInt - 48)
+      case _  => Sector.Free
+    .toList
+    
+
+  test("Day9 part1 expand example 1"):
+    assertEquals(expand(input), parse("00...111...2...333.44.5555.6666.777.888899"))
+
+  test("Day9 part1 expand example 2"):
+    assertEquals(expand("12345"), parse("0..111....22222"))
+
+  test("Day9 part1 compact example 1"):
+    assertEquals(compact(parse("00...111...2...333.44.5555.6666.777.888899")), parse("0099811188827773336446555566.............."))
+
+  test("Day9 part1 compact example 2"):
+    assertEquals(compact(parse("0..111....22222")), parse("022111222......"))
+
+  test("Day9 part1 checksum example 1"):
+    assertEquals(checksum(parse("0099811188827773336446555566..............")), 1928L)
 
   test("Day9 part1"):
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 1928L)
 
   test("Day9 part2"):
     assertEquals(part2(input), 1)

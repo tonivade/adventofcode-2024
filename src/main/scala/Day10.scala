e56c62bf7e0786f91e2cd72bc9d9f90914fb59e0
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-11 21:04:04 +0100

    day10 part1 WIP

---
+++ src/main/scala/Day10.scala
@@ -0,0 +1,56 @@
+package day10
+
+import scala.io.Source
+import scala.annotation.tailrec
+
+// https://adventofcode.com/2024/day/10
+object Day10:
+
+  case class Position(x: Int, y: Int):
+    def up = Position(x, y + 1)
+    def down = Position(x, y - 1)
+    def right = Position(x + 1, y)
+    def left = Position(x - 1, y)
+    def adjacent = List(up, down, left, right)
+
+  enum Tree:
+    case Leaf(value: Position)
+    case Node(value: Position, branches: List[Tree])
+    def leaves: List[Position] = 
+      this match
+        case Leaf(value) => List(value)
+        case Node(_, branches) => branches.flatMap(_.leaves)
+      
+  import Tree._
+
+  def parse(input: String): Map[Position, Int] = 
+    input.linesIterator.zipWithIndex.flatMap:
+      case (line, y) => line.zipWithIndex.map:
+        case (ch, x) => Position(x, y) -> (ch.toInt - 48)
+    .toMap
+
+  def search(matrix: Map[Position, Int])(start: Position): Tree = 
+    val next = start.adjacent.filter(matrix.contains).filter(p => matrix(p) - matrix(start) == 1)
+
+    if (next.isEmpty)
+      Leaf(start)
+    else
+      Node(start, next.map(search(matrix)))
+
+  def part1(input: String): Int = 
+    val matrix = parse(input)
+
+    val zeros = matrix.filter:
+      case (p, i) => i == 0
+    .keySet.toList
+
+    zeros.map(search(matrix)).foreach(println)
+    zeros.map(search(matrix)).flatMap(_.leaves).filter(matrix(_) == 9).size
+
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day10.txt").getLines().mkString("\n")
+  println(Day10.part1(input))
+  println(Day10.part2(input))
+
---
+++ src/test/scala/Day10Suite.scala
@@ -0,0 +1,21 @@
+package day10
+
+import Day10._
+
+class Day10Suite extends munit.FunSuite:
+
+  val input = """89010123
+                |78121874
+                |87430965
+                |96549874
+                |45678903
+                |32019012
+                |01329801
+                |10456732""".stripMargin
+
+  test("Day10 part1"):
+    assertEquals(part1(input), 36)
+
+  test("Day10 part2".ignore):
+    assertEquals(part2(input), 1)
+

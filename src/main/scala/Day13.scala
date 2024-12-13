ad6ebbcd923900c94a4d5ec3628b41a84dd98ee7
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-13 09:54:43 +0100

    day13 scafolding

---
+++ src/main/scala/Day13.scala
@@ -0,0 +1,14 @@
+package day13
+
+import scala.io.Source
+
+// https://adventofcode.com/2024/day/13
+object Day13:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day13.txt").getLines().mkString("\n")
+  println(Day13.part1(input))
+  println(Day13.part2(input))
+
---
+++ src/test/scala/Day13Suite.scala
@@ -0,0 +1,28 @@
+package day13
+
+import Day13._
+
+class Day13Suite extends munit.FunSuite:
+
+  val input = """Button A: X+94, Y+34
+                |Button B: X+22, Y+67
+                |Prize: X=8400, Y=5400
+                |
+                |Button A: X+26, Y+66
+                |Button B: X+67, Y+21
+                |Prize: X=12748, Y=12176
+                |
+                |Button A: X+17, Y+86
+                |Button B: X+84, Y+37
+                |Prize: X=7870, Y=6450
+                |
+                |Button A: X+69, Y+23
+                |Button B: X+27, Y+71
+                |Prize: X=18641, Y=10279""".stripMargin
+
+  test("Day13 part1".ignore):
+    assertEquals(part1(input), 1)
+
+  test("Day13 part2".ignore):
+    assertEquals(part2(input), 1)
+

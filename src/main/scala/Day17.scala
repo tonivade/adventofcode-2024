df495f0fc84b477999e6a341594397779d73c773
Antonio Muñoz <antonio.munoz@clarity.ai>
2024-12-19 16:12:43 +0100

    day17 part2 refactor

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -61,14 +61,13 @@ object Day17:
     exec(program)(computer).output.mkString(",")
 
   def solve2(program: Vector[Int]): Long = 
-    val candidates = program.reverseIterator.foldLeft(Set(0L)):
+    program.reverseIterator.foldLeft(Set(0L)):
       case (values, digit) =>
-        values.map(_ * 8L).flatMap: a =>
+        values.map(_ * 8).flatMap: a =>
           (a to a + 8).filter: candidate =>
             val output = exec(program)(Computer(candidate, 0, 0)).output
             output(0) == digit
-    
-    candidates.filter: a => 
+    .filter: a => 
       program.startsWith(exec(program)(Computer(a, 0, 0)).output)
     .min
 

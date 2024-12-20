3bb39979c6110556d5f879d73f81e16a73fa90f2
Antonio Muñoz <antoniogmc@gmail.com>
2024-12-21 00:47:08 +0100

    day18 part1

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -1,10 +1,59 @@
 package day18
 
 import scala.io.Source
+import scala.collection.mutable.PriorityQueue
+import scala.collection.mutable.HashSet
+import scala.util.boundary
+import scala.collection.mutable.Queue
 
 // https://adventofcode.com/2024/day/18
 object Day18:
-  def part1(input: String): Int = ???
+
+  case class Position(x: Int, y: Int):
+    def up = Position(x, y - 1)
+    def down = Position(x, y + 1)
+    def left = Position(x - 1, y)
+    def right = Position(x + 1, y)
+    def adjacent(bounds: Bounds) = List(up, down, left, right).filter(bounds.contains)
+ 
+  case class Bounds(width: Int, height: Int):
+    def start = Position(0, 0)
+    def stop = Position(width - 1, height - 1)
+    def contains(position: Position): Boolean = 
+      (0 until width).contains(position.x) && (0 until height).contains(position.y)
+
+  case class Node(position: Position, cost: Int):
+    def vertices(bounds: Bounds) = position.adjacent(bounds).map(Node(_, cost + 1))
+
+  def parse(input: String): List[Position] =
+    input.linesIterator.map(_.split(",")).map:
+      case Array(x, y) => Position(x.toInt, y.toInt)
+    .toList
+
+  def search(bytes: Set[Position], bounds: Bounds): Int =
+    val queue = Queue.empty[Node]
+    val visited = HashSet.empty[Position]
+
+    queue.enqueue(Node(bounds.start, 0))
+
+    var cost = Int.MaxValue
+    while (!queue.isEmpty)
+      val current = queue.dequeue()
+      println(current)
+      if (current.position == bounds.stop && cost > current.cost)
+        cost = current.cost
+      else if (visited.add(current.position))
+        current.vertices(bounds)
+          .filterNot(n => bytes.contains(n.position))
+          .foreach(queue.enqueue(_))
+    cost
+
+  def solve1(input: String, bounds: Bounds, number: Int): Int =
+    val bytes = parse(input)
+    search(bytes.take(number).toSet, bounds)
+
+  def part1(input: String): Int = solve1(input, Bounds(71, 71), 1024)
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day18Suite.scala
+++ src/test/scala/Day18Suite.scala
@@ -4,10 +4,34 @@ import Day18._
 
 class Day18Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """5,4
+                |4,2
+                |4,5
+                |3,0
+                |2,1
+                |6,3
+                |2,4
+                |1,5
+                |0,6
+                |3,3
+                |2,6
+                |5,1
+                |1,2
+                |5,5
+                |2,5
+                |6,5
+                |1,4
+                |0,4
+                |6,4
+                |1,1
+                |6,1
+                |1,0
+                |0,5
+                |1,6
+                |2,0""".stripMargin
 
-  test("Day18 part1".ignore):
-    assertEquals(part1(input), 1)
+  test("Day18 part1"):
+    assertEquals(solve1(input, Bounds(7, 7), 12), 22)
 
   test("Day18 part2".ignore):
     assertEquals(part2(input), 1)
